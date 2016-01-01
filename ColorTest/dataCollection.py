d5ce972506831a6cd2d7006c7d4703efb6ec171e
Sudip Guha <sudipguha29@gmail.com>
2015-12-31 22:32:11 -0800

    added color correction and label logging

--- ColorTest/dataCollection.py
+++ ColorTest/dataCollection.py
@@ -12,10 +12,11 @@ import sys
 import math
 from scipy import ndimage
 import matplotlib.image as mpimg
-import pygame
 from PIL import Image, ImageTk
 import pdb
 import copy
+import colorcorrect.algorithm as cca
+from colorcorrect.util import from_pil, to_pil
 
 
 class App:
@@ -39,17 +40,13 @@ class App:
 		self.button.grid(row=2,column=0)
 
 		self.fileName = ""
-
-		f = Image.open("rs9.png")
-		photo = ImageTk.PhotoImage(f)
-		imgRGB = cv2.cvtColor(cv2.imread('rs9.png'), cv2.COLOR_BGR2RGB)
-
+		self.imgDisplayed = None
 		self.colors = ["red", "brown", "blue", "black","brown","yellow","violet","green"]
 
 
 		self.canvas = Canvas(frame, width=900, height=900, cursor="cross")		
-		self.imgDisplayed = self.canvas.create_image(0,0,image=photo, anchor='nw', state=NORMAL)
-		self.canvas.image = photo 
+		#self.imgDisplayed = self.canvas.create_image(0,0,image=photo, anchor='nw', state=NORMAL)
+		#self.canvas.image = photo 
 		self.canvas.grid(row=0,column=0)
 
 		self.current_color = ""
@@ -67,6 +64,14 @@ class App:
 			self.color_selectors[color] = Button(button_frame, text=color, command=self.color_selectors_fn[color])
 			self.color_selectors[color].grid(row=1, column=self.colors.index(color))
 
+
+		"""
+		Data Structures for labels
+		"""
+		self.labels = dict() #disctionary color: list of labeled points
+
+
+
 	def set_color(self, color):
 		clr = copy.deepcopy(color)
 		
@@ -83,24 +88,35 @@ class App:
 		x,y = int(x),int(y)
 		canvas.create_oval(max(0,x-1),max(0,y-1),min(canvas.winfo_width(),x+1),min(canvas.winfo_height(),y+1), fill="red")
 		print str(x) + " " + str(y) + " " + self.current_color
+		if not self.current_color in self.labels.keys():
+			self.labels[self.current_color] = [(x, y)]
+		else:
+			self.labels[self.current_color].append((x,y))
 
 	def openFile(self):
 		self.fileName = tkFileDialog.askopenfilename(parent=self.root)
 		print self.fileName
 		self.canvas.delete(self.imgDisplayed)
-		f = Image.open(self.fileName)
+		f = Image.open('rs9.png')
+		f = to_pil(cca.stretch(from_pil(f)))
 		photo = ImageTk.PhotoImage(f)
 		self.imgDisplayed = self.canvas.create_image(0,0,image=photo, anchor='nw', state=NORMAL)
 		self.canvas.image = photo 
 		self.canvas.grid(row=0,column=0)
 		imgRGB = cv2.cvtColor(cv2.imread('rs9.png'), cv2.COLOR_BGR2RGB)
-		
+
 
 	def close(self):
 		self.root.destroy()
 
 	def saveFile(self):
 		targetFileName = self.fileName.split(".")[0]+".data"
+		f = open(targetFileName, 'a')
+		for color in self.labels.keys():
+			for (x,y) in self.labels[color]:
+				f.write(str(x)+","+str(y)+","+color+"\n")
+		print "data (coords and labels) saved at "+targetFileName
+		f.close()
 
 
 
