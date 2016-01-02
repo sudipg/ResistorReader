2da64d402efe2eb956cd0ff2ef5787995b597706
Sudip Guha <sudipguha29@gmail.com>
2016-01-02 04:36:46 -0800

    starting pixel classification

--- image_analysis.py
+++ image_analysis.py
@@ -10,22 +10,28 @@ from scipy import ndimage
 from transforms import *
 from colors import *
 import matplotlib.image as mpimg
+from mpl_toolkits.mplot3d import Axes3D
 import pygame
+import glob
 
 def ColorMatch(RGB):
 	return RGBColorMatch(RGB[0],RGB[1],RGB[2])
 
 def draw(img):
+	fig = plt.figure()
 	plt.imshow(img)
 	plt.show()
+	return fig
 
-source = 'clr_test1.JPG'
+sources = glob.glob("ColorLabels/*.data")
 
-img_RGB = cv2.blur(cv2.cvtColor((cv2.imread('images/'+source,1)), cv2.COLOR_BGR2RGB), (2,2))
+for source in sources:
 
-img_LAB = cv2.blur(cv2.cvtColor((cv2.imread('images/'+source,1)), cv2.COLOR_BGR2LAB), (2,2))
+	img_RGB = cv2.blur(cv2.cvtColor(cv2.imread('images/'+source,1), cv2.COLOR_BGR2RGB), (2,2))
 
-img = mpimg.imread('images/'+source)
+	img_LAB = cv2.blur(cv2.cvtColor(cv2.imread('images/'+source,1), cv2.COLOR_BGR2LAB), (2,2))
+
+	img = mpimg.imread('images/'+source)
 
 brown = [(240, 230), (236,245), (237,280)]
 black = [(237,266)]
